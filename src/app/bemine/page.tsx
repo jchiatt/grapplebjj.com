9f4c1ba97d6e82d5aa28bdf10a88aab81c6c636b
J.C. Hiatt <jc@jchiatt.com>
2025-01-26 21:58:03 -0600

    fix grid cols

--- src/app/bemine/page.tsx
+++ src/app/bemine/page.tsx
@@ -171,7 +171,7 @@ export default function BeminePage() {
         <div className="mt-8 p-8 rounded-lg bg-white dark:bg-black shadow-lg">
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <div className="grid grid-cols-2 gap-4">
+              <div className="grid md:grid-cols-2 gap-4">
                 <FormField
                   control={form.control}
                   name="couponCode"
