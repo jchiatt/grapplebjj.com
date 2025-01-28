e213ef8d9038b96eac00237925c396db85401969
J.C. Hiatt <jc@jchiatt.com>
2025-01-28 01:14:36 -0600

    add bemine layout

---
+++ src/app/bemine/layout.tsx
@@ -0,0 +1,11 @@
+import { metadata } from "./metadata";
+
+export { metadata };
+
+export default function BemineLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
