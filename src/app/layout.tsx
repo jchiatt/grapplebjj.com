43c6dd79c90a10772146a3a3fb019fbccd18f0ca
J.C. Hiatt <jc@jchiatt.com>
2025-01-28 00:35:08 -0600

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,16 @@ const metrophobic = Metrophobic({
 export const metadata: Metadata = {
   title: "Grapple Jiu Jitsu",
   description: "No-Gi submission grappling gym in Pearl, Mississippi.",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
