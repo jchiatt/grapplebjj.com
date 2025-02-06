8a1147173e93e7bafea79cee49d06b5fe88536fd
J.C. Hiatt <jc@jchiatt.com>
2025-02-06 14:29:49 -0600

    add youtube redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   async redirects() {
     return [
+      {
+        source: "/youtube",
+        destination: "https://www.youtube.com/@grppletheworld",
+        permanent: false,
+      },
       // {
       //   source: "/trial",
       //   destination: "https://grapple.gymdesk.com/signup?membership_id=6824",
